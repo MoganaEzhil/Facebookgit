1502a622e6c6d658c04073a1a6f8a6ab2fb13cfb
MoganaEzhil <moganaarch2000@gmail.com>
2021-12-31 16:32:28 +0530

    Username Functionality

--- src/org/fbgit/Fbgitclass.java
+++ src/org/fbgit/Fbgitclass.java
@@ -1,14 +1,14 @@
 package org.fbgit;
 
 public class Fbgitclass {
-	int a;
-	private void empId() {
-System.out.println(a);
+	public static void empId() {
+		System.out.println("empId is 200");
+
 	}
 	public static void main(String[] args) {
-		Fbgitclass f =new Fbgitclass();
-		f.empId();
-		
+		empId();
 	}
+		
+	
 
 }
